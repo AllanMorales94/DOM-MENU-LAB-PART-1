bdeaaa607c106683e1a118170fe23ee063f9b70f
AllanMorales94 <overlydedicated1994@gmail.com>
2023-06-10 14:34:04 -0700

    Task 1.2 Set the content of mainEl with an <h1> to the text of your preference

--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ const mainEl = document.querySelector('main')
 
 // Task 1.1 using style to link our color from our css file
 mainEl.style.backgroundColor = 'var(--main-bg)'
+
+// Task 1.2 using innerHTML to update our html with a new element 
+mainEl.innerHTML = '<h1>Polish Your Mastering Skills</h1>'
