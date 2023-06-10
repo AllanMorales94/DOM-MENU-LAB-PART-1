a8d280040e7cb3cbf51cc61563b717aa0435e737
AllanMorales94 <overlydedicated1994@gmail.com>
2023-06-10 14:26:15 -0700

    Task 1.1 Set the background color of mainEl using the --main-bg CSS custom property.Assign a string that uses the CSS var() function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 // Task 1.0 selecting our main element from our document 
 const mainEl = document.querySelector('main')
+
+// Task 1.1 using style to link our color from our css file
+mainEl.style.backgroundColor = 'var(--main-bg)'
