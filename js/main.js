c4769ae089cf25c2dd77aed51acbd817af6f345e
AllanMorales94 <overlydedicated1994@gmail.com>
2023-07-09 18:48:35 -0700

    Refactor code

--- js/main.js
+++ js/main.js
@@ -91,13 +91,14 @@ topMenuEl.addEventListener('click', function(evt) {
   evt.preventDefault();
   const link = evt.target;
   if (link.tagName !== 'A') return;
-  console.log(link.textContent);
-});
+  
+
 
 // Task 5.3
-if(link.classList.contains('active')) {
-  link.classList.remove('active')
-  showingSubMenu = false
-  subMenuEl.style.top = '0'
-  return
+if (link.classList.contains('active')) {
+  link.classList.remove('active');
+  showingSubMenu = false;
+  subMenuEl.style.top = '0';
+  return;
 }
+})
