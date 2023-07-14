f61dd220042063ca56cc1bf0c6d9fb656d9e631e
AllanMorales94 <overlydedicated1994@gmail.com>
2023-07-14 11:20:17 -0700

    Implement code at the end of the event listener, which iterates through each anchor element in topMenuLinks regardless of whether the anchor element currently has that class or not

--- js/main.js
+++ js/main.js
@@ -91,14 +91,17 @@ topMenuEl.addEventListener('click', function(evt) {
   evt.preventDefault();
   const link = evt.target;
   if (link.tagName !== 'A') return;
-  
 
+  // Task 5.3
+  if (link.classList.contains('active')) {
+    link.classList.remove('active');
+    showingSubMenu = false;
+    subMenuEl.style.top = '0';
+    return;
+  }
 
-// Task 5.3
-if (link.classList.contains('active')) {
-  link.classList.remove('active');
-  showingSubMenu = false;
-  subMenuEl.style.top = '0';
-  return;
-}
-})
+  // Task 5.4 
+  topMenuLinks.forEach(function(link) {
+    link.classList.remove('active');
+  });
+});
